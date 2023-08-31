8279a969cf445a81645bdd0f92a5c2f257e02469
Pavlo Honcharenko <pavelgonchar13@gmail.com>
2023-08-31 22:46:41 +0300

    slice_done

---
+++ src/redux/filterSlice.js
@@ -0,0 +1,18 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialState = {
+    filters : []
+}
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+        setStatusFilter : (state, action) => {
+        state.filters.push(action.payload)
+        }
+    }
+})
+
+export const { setStatusFilter } = filterSlice.actions;
+export const filtersReducer = filterSlice.reducer;
---
+++ src/redux/store.js
@@ -0,0 +1,39 @@
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import { itemsReducer } from "./contactsSlice";
+import { filtersReducer } from "./filterSlice";
+import storage from 'redux-persist/lib/storage';
+
+const { combineReducers, configureStore } = require("@reduxjs/toolkit");
+
+const rootReducer = combineReducers({
+    filter: filtersReducer,
+    items:itemsReducer,
+})
+
+const persistConfig = {
+    key: "root",
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store)
